b27f67797410fde21abc8e709d04c2a40db73727
Martin Kalany <m.kalany@gmx.at>
2013-08-13 16:26:51 +0200

    add ínterface for graph generator

---
+++ src/generator.cpp
@@ -0,0 +1,12 @@
+
+#include "generator.hpp"
+
+int Generator::generate_rDAG(size_t n, size_t m, bool allow_parallel_edges, size_t d, int **weights, std::ostream out)
+{
+   return -1;
+}
+
+int Generator::generate_rDiGraph(size_t n, size_t m, bool allow_parallel_edges, size_t d, int **weights, std::ostream out)
+{
+   return -1;
+}
