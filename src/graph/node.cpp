1a81f5b67cceef59eceed2125a97aa3e9e9c3544
Jakob Gruber <jakob.gruber@gmail.com>
2013-08-14 14:46:47 +0200

    Node: id() and out_edges()

--- src/graph/node.cpp
+++ src/graph/node.cpp
@@ -3,6 +3,26 @@
 namespace graph
 {
 
+ulong
+Node::
+id() const
+{
+    return AGID(n);
+}
+
+std::vector<Edge *>
+Node::
+out_edges() const
+{
+    std::vector<Edge *> edges;
+
+    for (Agedge_t *e = agfstout(graph->g, n); e != nullptr; e = agnxtout(graph->g, e)) {
+        edges.push_back(graph->get_edge(AGID(e)));
+    }
+
+    return edges;
+}
+
 Node::
 Node(Graph *graph)
     : graph(graph)
--- src/graph/node.h
+++ src/graph/node.h
@@ -1,6 +1,8 @@
 #ifndef __NODE_H
 #define __NODE_H
 
+#include <vector>
+
 #include "graph.h"
 
 namespace graph
@@ -10,6 +12,11 @@ class Node
 {
     friend class Edge;
     friend class Graph;
+
+public:
+    ulong id() const;
+
+    std::vector<Edge *> out_edges() const;
 
 private:
     Node(Graph *graph);
