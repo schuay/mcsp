2cf6f9da17649cf26a5373e4346a0735858de424
Jakob Gruber <jakob.gruber@gmail.com>
2013-08-14 15:13:20 +0200

    Add command line args to generator_test

--- src/generator/generator_test.cpp
+++ src/generator/generator_test.cpp
@@ -1,22 +1,52 @@
+#include <getopt.h>
 #include <stdio.h>
 
 #include "generator.h"
 
 using namespace graph;
 
-int
-main(int argc __attribute__((unused)),
-     char **argv __attribute__((unused)))
+static void
+usage(void)
 {
+    fprintf(stderr, "gengraph [-n <# nodes>] [-m <# edges>] [-s <seed>]\n");
+    exit(EXIT_FAILURE);
+}
 
-    graph::Generator::Wl wl;
+int
+main(int argc,
+     char **argv)
+{
+    int n = 10;
+    int m = 15;
+    int s = 42;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "n:m:s:")) != -1) {
+        switch (opt) {
+        case 'm':
+            m = strtol(optarg, NULL, 10);
+            break;
+        case 'n':
+            n = strtol(optarg, NULL, 10);
+            break;
+        case 's':
+            s = strtol(optarg, NULL, 10);
+            break;
+        default:
+            usage();
+        }
+    }
+
+    Generator::Wl wl;
     wl.push_back(std::pair<int, int>(0, 1));
     wl.push_back(std::pair<int, int>(3, 10));
     wl.push_back(std::pair<int, int>(-2, 2));
-    Graph *g = graph::Generator::generate_rDiGraph(std::string("test graph"), 10, 15, true, wl,
-               (unsigned short) time(NULL));
+
+    Graph *g = Generator::generate_rDiGraph("test graph", n, m, true, wl, s);
 
     g->write(stdout);
+
+    delete g;
 
     return 0;
 }
