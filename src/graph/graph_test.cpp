b06612b64092f312a530f4d4c67bab726cd47876
Jakob Gruber <jakob.gruber@gmail.com>
2013-08-14 09:32:47 +0200

    A simple graph main function
    Just to catch additional errors for now.

---
+++ src/graph/graph_test.cpp
@@ -0,0 +1,16 @@
+#include "graph.h"
+#include "node.h"
+#include "edge.h"
+
+int
+main(int argc,
+     char **argv)
+{
+    graph::Graph g("g");
+
+    graph::Node *tail = new graph::Node(&g);
+    graph::Node *head = new graph::Node(&g);
+    graph::Edge *e = new graph::Edge(tail, head, { 1, 3, 5 });
+
+    return 0;
+}
