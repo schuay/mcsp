13500cb88e2e018a0151aaff0226b09183071a76
Jakob Gruber <jakob.gruber@gmail.com>
2013-08-14 11:32:05 +0200

    Pack and unpack weights

--- src/graph/edge.cpp
+++ src/graph/edge.cpp
@@ -1,6 +1,7 @@
 #include "edge.h"
 
 #include <assert.h>
+#include <sstream>
 
 namespace graph {
 
@@ -15,6 +16,12 @@ Edge(Node *tail,
     graph = tail->graph;
     e = agedge(graph->g, tail->n, head->n, NULL, TRUE);
     graph->add_edge(AGID(e), this);
+
+    std::stringstream ss;
+    for (auto i : weights) {
+        ss << i << " ";
+    }
+    agset(e, ATTR_WEIGHT, const_cast<char *>(ss.str().c_str()));
 }
 
 Edge::
@@ -25,7 +32,11 @@ Edge(Graph *graph,
     tail = graph->get_node(AGID(agtail(e)));
     head = graph->get_node(AGID(aghead(e)));
 
-    /* TODO: Restore weights. */
+    std::stringstream ss(std::string(agget(e, ATTR_WEIGHT)));
+    int w;
+    while (ss >> w) {
+        weights.push_back(w);
+    }
 }
 
 }
