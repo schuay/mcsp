67642319627806a170cd629cee2dda96fb44d532
Jakob Gruber <jakob.gruber@gmail.com>
2013-08-21 14:53:01 +0200

    Test for presence of all reachable nodes

--- test/sequential/sequential.cpp
+++ test/sequential/sequential.cpp
@@ -1,3 +1,6 @@
+#include <stack>
+#include <unordered_set>
+
 #include "generator/generator.h"
 #include "gtest/gtest.h"
 #include "pareto/less.h"
@@ -37,6 +40,33 @@ is_optimal_path_set(const Paths &paths)
     return true;
 }
 
+std::unordered_set<const Node *>
+reachable_nodes(const Node *start)
+{
+    std::unordered_set<const Node *> set = { start };
+
+    std::stack<const Node *> stack;
+    for (const Edge * e : start->out_edges()) {
+        stack.push(e->head());
+    }
+
+    while (!stack.empty()) {
+        const Node *n = stack.top();
+        stack.pop();
+
+        if (set.find(n) != set.end()) {
+            continue;
+        }
+
+        set.insert(n);
+        for (const Edge * e : n->out_edges()) {
+            stack.push(e->head());
+        }
+    }
+
+    return set;
+}
+
 TEST(TESTCASE, SanityCheck)
 {
     Graph *g = Generator::directed("g", NODES, EDGES, true,
@@ -72,6 +102,35 @@ TEST(TESTCASE, ContainsOptimalPathsOnly)
     delete sp;
     delete g;
 }
+
+TEST(TESTCASE, AllReachableNodesPresent)
+{
+    Graph *g = Generator::directed("g", NODES, EDGES, true,
+                                   Generator::default_weights(), SEED);
+    ASSERT_NE(g, nullptr);
+
+    Node *node = g->nodes().front();
+    ASSERT_NE(node, nullptr);
+
+    Sequential seq(g, node);
+    ShortestPaths *sp = seq.shortest_paths();
+
+    std::unordered_set<const Node *> reachables = reachable_nodes(node);
+
+    for (const auto & node_paths_pair : sp->paths) {
+        reachables.erase(node_paths_pair.first);
+    }
+
+    EXPECT_TRUE(reachables.empty());
+
+    delete sp;
+    delete g;
+}
+
+/* TODO:
+ * Repeat these tests for different NODES, EDGES, SEED values.
+ * Add other correctness criteria.
+ */
 
 } /* namespace */
 
