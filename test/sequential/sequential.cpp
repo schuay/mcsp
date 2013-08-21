2820d8e29ecd79e61f2fce372e00340ef9da8c59
Jakob Gruber <jakob.gruber@gmail.com>
2013-08-21 14:29:29 +0200

    Test for dominated paths in result set
    This test exposes an error in the current implementation.

--- test/sequential/sequential.cpp
+++ test/sequential/sequential.cpp
@@ -1,8 +1,10 @@
 #include "generator/generator.h"
 #include "gtest/gtest.h"
+#include "pareto/less.h"
 #include "sequential/sequential.h"
 
 using namespace graph;
+using namespace pareto;
 using namespace sp;
 
 #define NODES (50)
@@ -13,6 +15,27 @@ using namespace sp;
 
 namespace
 {
+
+bool
+is_optimal_path_set(const Paths &paths)
+{
+    less dominates;
+
+    for (size_t i = 0; i < paths.size(); i++) {
+        for (size_t j = i + 1; j < paths.size(); j++) {
+            const Path *l = paths[i].get();
+            const Path *r = paths[j].get();
+
+            if (dominates(l, r) || dominates(r, l)) {
+                l->print();
+                r->print();
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
 
 TEST(TESTCASE, SanityCheck)
 {
@@ -25,6 +48,26 @@ TEST(TESTCASE, SanityCheck)
 
     Sequential seq(g, node);
     ShortestPaths *sp = seq.shortest_paths();
+
+    delete sp;
+    delete g;
+}
+
+TEST(TESTCASE, ContainsOptimalPathsOnly)
+{
+    Graph *g = Generator::directed("g", NODES, EDGES, true,
+                                   Generator::default_weights(), SEED);
+    ASSERT_NE(g, nullptr);
+
+    Node *node = g->nodes().front();
+    ASSERT_NE(node, nullptr);
+
+    Sequential seq(g, node);
+    ShortestPaths *sp = seq.shortest_paths();
+
+    for (const auto & node_paths_pair : sp->paths) {
+        EXPECT_TRUE(is_optimal_path_set(node_paths_pair.second));
+    }
 
     delete sp;
     delete g;
